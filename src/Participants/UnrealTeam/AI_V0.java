a750456d9b399250723258cd18ee400f100fc206
Horia Mut <horia.mut@net2000.ch>
2015-10-28 12:04:01 +0100

    Finish the AI.

--- src/Participants/UnrealTeam/AI_V0.java
+++ src/Participants/UnrealTeam/AI_V0.java
@@ -5,12 +5,119 @@
  */
 package Participants.UnrealTeam;
 
+import Othello.Move;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  *
- * @author The Gazou
+ * @author Nicolas Gonin, Horia Mut
  */
-public class AI_V0 {
-    
-    
-    
+public class AI_V0
+{
+
+    // Attributes
+    public static final int INF = Integer.MAX_VALUE;
+    private int player;
+    private int depth;
+
+    public static final int MIN = -1;
+    public static final int MAX = 1;
+
+    /**
+     * AI constructor.
+     *
+     * @param playerID
+     * @param depth
+     */
+    public AI_V0(int playerID, int depth)
+    {
+	this.player = playerID;
+	this.depth = depth;
+    }
+
+    public int evaluationFunction(GameBoard board, int playerID)
+    {
+	return board.getCoinCount(playerID);
+    }
+
+    public void buildTree(GameBoard gameBoard, Node root, int playerID)
+    {
+	// Fill the tree with our possible moves.
+	ArrayList<Move> allOurPossibleMoves = gameBoard.getPossibleMoves(playerID);
+	for (Iterator<Move> iMove = allOurPossibleMoves.iterator(); iMove.hasNext();)
+	    {
+	    Move next = iMove.next();
+	    // Build the node.
+	    Node nextMoveNode = new Node(next);
+	    // Set it's data.
+	    nextMoveNode.setEvaluation(evaluationFunction(gameBoard, playerID));
+	    root.addChildNode(nextMoveNode);
+	    }
+    }
+
+    public Move getBestMove(GameBoard gameBoard, Node root, int depth, int playerID)
+    {
+	return alphaBetaN(gameBoard, root, depth, MAX, root.getEvaluation(), playerID).getMove();
+    }
+
+    private Node alphaBetaN(GameBoard board, Node root, int depth, int minOrMax, int parentValue, int currentPlayer)
+    {
+
+	// Check if we have reached the maximum depth of if the root is at the end.
+	if (depth == 0 || root.isLeaf())
+	    {
+            // Since we have reached the end, evaluate the actual node (root).
+	    // Root should contain a move!
+	    root.setEvaluation(evaluationFunction(board, player));
+	    if (root.getMove() == null)
+		{
+		System.err.println("No move,what the fuck?");
+		}
+	    return root;
+	    }
+
+	int optVal = minOrMax * -INF;
+	// No optimal operation (move) yet.
+	Node optMove = new Node(null);
+
+	ArrayList<Node> allPossibleMoves = root.getChildNodeList();
+
+	// Check if we can do any moves.
+	if (allPossibleMoves.isEmpty())
+	    {
+	    // If we can't, we skip our move.
+	    optVal = alphaBetaN(board, root, depth - 1, -minOrMax, optVal, -currentPlayer).getEvaluation();
+	    } else
+	    {
+	    while (!allPossibleMoves.isEmpty())
+		{
+
+		// Get the node containing the first possible move.
+		Node actualMoveNode = allPossibleMoves.remove(0);
+
+		// Now rebuild the game state, add the piece, then re-evaluate while substracting the depth.
+		GameBoard gameBoardClone = board.clone();
+		gameBoardClone.addCoin(actualMoveNode.getMove(), currentPlayer);
+
+		// Once the coin has been added, do alpha beta on the new Board and minimize this time.
+		actualMoveNode.setEvaluation(alphaBetaN(gameBoardClone, actualMoveNode, depth - 1, -minOrMax, optVal, currentPlayer).getEvaluation());
+		int val = actualMoveNode.getEvaluation();
+		if (val * minOrMax > optVal * minOrMax)
+		    {
+		    optVal = val;
+		    optMove.setMove(actualMoveNode.getMove());
+		    optMove.setEvaluation(optVal);
+		    if (optVal * minOrMax > parentValue * minOrMax)
+			{
+			break;
+			}
+		    }
+		}
+	    }
+
+	optMove.setEvaluation(optVal);
+	return optMove;
+    }
+
 }
--- src/Participants/UnrealTeam/Joueur.java
+++ src/Participants/UnrealTeam/Joueur.java
@@ -2,42 +2,74 @@ package Participants.UnrealTeam;
 
 import Othello.Move;
 
-
 /**
- * Managages the player
+ * Managage the player
+ *
  * @author Ellenberger Patrick and Moll Adrian
  *
  */
-public class Joueur extends Othello.Joueur{
+public class Joueur extends Othello.Joueur
+{
 
-	private int depth;
-	private int enemyID;
-	private GameBoard gameBoard;
-	
-	
-	public Joueur(int depth, int playerID) {
-		super();
-		this.depth = depth;
-		this.playerID = playerID;
-		this.enemyID = 1-playerID;
-		this.gameBoard = new GameBoard();
-	}
+    private int depth;
+    private int enemyID;
+    private GameBoard gameBoard;
 
+    private AI_V0 ourAI;
 
-	/**
-	 * Method called every time the player has to play
-	 */
-	public Move nextPlay(Move move) {
-		
-		//Add enemy coin to the gameboard
-		gameBoard.addCoin(move, enemyID);
-		
-		//Get the best move (null if no move possible)
-		Move bestMove = AI_V0.getBestMove(gameBoard, depth, playerID);
-		
-		//Add player coin to the gameboard
-		gameBoard.addCoin(bestMove, playerID);
-		return bestMove;
-	}
+    public Joueur(int depth, int playerID)
+    {
+	super(depth, playerID);
+	this.depth = depth;
+	this.playerID = playerID;
+	this.enemyID = 1 - playerID;
+	this.gameBoard = new GameBoard();
+	this.ourAI = new AI_V0(playerID, depth);
+    }
+
+    /**
+     * Method called every time the player has to play
+     */
+    @Override
+    public Move nextPlay(Move move)
+    {
+
+	// Ici, vous devrez
+	// - Mettre à jour votre représentation du jeu en fonction du coup joué par l'adversaire
+	// - Décider quel coup jouer (alpha-beta!!)
+	// - Remettre à jour votre représentation du jeu
+	// - Retourner le coup choisi
+	Move enemyMove = null;
+
+	// Build the root of the algorithm tree.
+	Node root = new Node(null);
+	root.setEvaluation(ourAI.evaluationFunction(gameBoard, playerID));
+
+	if (move != null)
+	    {
+	    //Add enemy coin to the gameboard
+	    gameBoard.addCoin(move, enemyID);
+	    enemyMove = move;
+
+	    // The enemy had a valid move. Add it to the tree.
+	    root.setMove(enemyMove);
+	    root.setEvaluation(ourAI.evaluationFunction(gameBoard, enemyID));
+	    }
+
+	// Build the tree.
+	ourAI.buildTree(gameBoard, root, playerID);
+
+	// Now start the IA.
+	// Get the best move (null if no move possible).
+	Move ourMove = ourAI.getBestMove(gameBoard, root, depth, playerID);
+
+	//Add player coin to the gameboard
+	if (ourMove != null)
+	    {
+	    this.gameBoard.addCoin(ourMove, playerID);
+	    }
+	return ourMove != null ? ourMove : null;
+
+    }
 
 }
